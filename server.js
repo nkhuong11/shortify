59057a97c92c4941bcbf6d8cfec9a6b3b7bf614b
Khuong Nguyen <khuong.nguyen0322@gmail.com>
2019-11-23 20:08:27 +0700

    deploying

--- server.js
+++ server.js
@@ -44,6 +44,17 @@ app.use('/', redirectMiddleware);
 app.use('/api/user', authenticateRoutes);
 app.use('/api/services/', servicesRoutes);
 
+if(process.env.NODE_ENV === 'production') {
+  const path = require('path');
+  //Express will server up production assets like main.js, main.css file
+  app.use(express.static(path.join(__dirname, 'client/build')));
+  //Express will server index.html file if it doesn't recognize the route
+  app.get('*', (req, res) => {
+    //res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.join(__dirname+'/client/build/index.html'));
+  });
+}
+
 const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
